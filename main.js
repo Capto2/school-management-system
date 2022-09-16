c93174a6ddb9a803ac1f82f1cca8411320f300df
Augustine Jemifor <113078564+Capto2@users.noreply.github.com>
2022-09-16 15:41:39 -0700

    Starting of the school management project
    Easy control of the school project

---
+++ main.js
@@ -0,0 +1,66 @@
+//jshint:e6
+//require modules
+const express = require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+const port = 3000;
+
+//represent modules
+const app = express();
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost/schooldb", {useNewUrlParser: true}); //mongodb connect
+
+const schoolSchema = new mongoose.Schema({
+    username: String,
+    studentId: String,
+    password: String,
+});
+
+app.route("/").get(function (request, response) {
+  response.render("home");
+});
+
+app
+  .route("/application")
+  .get(function (request, response) {
+    response.render("application");
+  })
+  .post(function (request, response) {
+    const email = request.body.email;
+    const password = request.body.password;
+  });
+
+app
+  .route("/login")
+  .get(function (request, response) {
+    response.render("login");
+  })
+  .post(function (request, response) {});
+app
+  .route("/enquires")
+  .get(function (request, response) {
+    response.render("enquires");
+  })
+  .post(function (request, response) {
+    const email = request.body.email;
+    const content = request.body.content;
+  });
+
+app
+  .route("/request-transcript")
+  .get(function (request, response) {
+    response.render("request-transcript");
+  })
+  .post(function (request, response) {
+    const student_data = request.body.student-data;
+    const email = request.body.email;
+  });
+
+app.listen(port, function () {
+  console.log("server has started on port 3000");
+});
