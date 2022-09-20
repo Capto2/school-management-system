08c6d77d7ec152f71e692aa6bdd93acbc73e3548
Augustine Jemifor <113078564+Capto2@users.noreply.github.com>
2022-09-20 02:38:36 -0700

    Added files worked on for two days

---
+++ READme.md
@@ -0,0 +1,2 @@
+The file named upload is for uploading of files for online clearance.
+The Study-course is where to register courses.
--- main.js
+++ main.js
@@ -6,12 +6,26 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const lodash = require("lodash");
 const port = 3000;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 //represent modules
 const app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "Our school websites.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost/schooldb", { useNewUrlParser: true }); //mongodb connect
 
@@ -21,7 +35,20 @@ const studentSchema = new mongoose.Schema({
   password: String,
 });
 
+studentSchema.plugin(passportLocalMongoose); //salt schema
+// studentSchema.plugin(findOrCreate);
+
 const studentModel = new mongoose.model("student", studentSchema);
+passport.use(studentModel.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  studentModel.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 app.route("/").get(function (request, response) {
   response.render("home");
@@ -35,6 +62,26 @@ app
   .post(function (request, response) {
     const email = request.body.email;
     const password = request.body.password;
+    // studentModel.register(
+
+    // );
+  });
+
+app
+  .route("/application/take-a-test")
+  .get(function (request, response) {
+    response.render("take-test");
+  })
+  .post(function (request, response) {});
+
+app
+  .route("/request-transcript")
+  .get(function (request, response) {
+    response.render("request-transcript");
+  })
+  .post(function (request, response) {
+    const student_data = request.body.student - data;
+    const email = request.body.email;
   });
 
 app
@@ -53,15 +100,49 @@ app
     const content = request.body.content;
   });
 
+app.get("/index", function (request, response) {
+  response.render("student-dashboard/home");
+});
 app
-  .route("/request-transcript")
+  .route("/index/clearance")
   .get(function (request, response) {
-    response.render("request-transcript");
+    response.render("student-dashboard/uploads");
   })
-  .post(function (request, response) {
-    const student_data = request.body.student - data;
-    const email = request.body.email;
-  });
+  .post(function (request, response) {});
+
+app
+  .route("/index/study-course")
+  .get(function (request, response) {
+    response.render("student-dashboard/study-course");
+  })
+  .post(function (request, response) {});
+
+app.route("/index/accomodation").get(function (request, response) {
+  response.render("student-dashboard/accomodation");
+});
+
+app
+  .route("/index/profile")
+  .get(function (request, response) {
+    response.render("student-dashboard/profile");
+  })
+  .post(function (request, response) {});
+
+app
+  .route("/index/message")
+  .get(function (request, response) {
+    response.render("student-dashboard/message");
+  })
+  .post(function (request, response) {});
+
+app.get("/index/check-result", function (request, response) {
+  response.render("student-dashboard/check-result");
+});
+
+app.get("/index/logout", function (request, response) {
+  request.logout();
+  response.redirect("/login");
+});
 
 app.listen(port, function () {
   console.log("server has started on port 3000");
